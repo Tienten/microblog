0bb6ce7bb9b0c0cb8a4ea3319d68baffdaef6d32
Tienten <108110186+Tienten@users.noreply.github.com>
2023-08-23 10:12:18 -0400

    Added flask_babel

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,8 +9,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
-
-
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -22,6 +21,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 
@@ -56,6 +56,9 @@ if not app.debug:
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
         
-        
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 
--- config.py
+++ config.py
@@ -12,4 +12,6 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['thuytienten@gmail.com']
     POSTS_PER_PAGE = 3
+    LANGUAGES = ['en','es']
+    
     
